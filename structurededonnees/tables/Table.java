3127a47a5e6d227392e1cf90f2e5056f34b73aaf
AdrienDucourthial <adu@evertrust.fr>
2023-03-17 16:20:03 +0100

    third commit

--- structurededonnees/tables/Table.java
+++ structurededonnees/tables/Table.java
@@ -7,26 +7,33 @@ import java.util.function.Function;
 public class Table<E> {
     private static Object[] table;
 
-    public static Table<E> of(Object... arg) {
-        table = new Object[arg.length];
+    private Table(Object[] table) {
+        this.table = table;
+    }
+
+    public static <E> Table<E> of(E... arg) {
         for (int i = 0; i < arg.length; i++) {
-            table[i] = arg;
+            if (arg[i] == null) {
+                throw new NullPointerException("I don't want null values");
+            }
         }
-        return (Table<E>) table;
+        return new Table<>(arg);
     }
 
     public int size() {
         return table.length;
     }
 
-    public Group groupBy(Function proj, Function comp) {
+    static <E> Group groupBy(Function proj, Comparable comp) {
         var g = new Group();
+        var tmp = comp(proj);
         return g;
     }
 
     public class Group {
+        public int[] indexes;
         public int keySize() {
-
+            return indexes.length;
         }
     }
 }
